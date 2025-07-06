849ec1700cd0df0bdfd0604669417a870605d292
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-07-06 23:20:12 +0200

    make the computer guess a number, print guess and attempts

--- game.py
+++ game.py
@@ -5,3 +5,16 @@ def computer_guess_game():
         x = int(input('Enter a maximum number: '))
     except ValueError:
         print('Invalid input! ')
+
+    low = 1
+    high = x
+    feedback = ''
+    attempts = 0
+    while feedback != 'c':
+        computer_guess = random.randint(low , high)
+        print(f'Computer guessed {computer_guess}')
+        feedback = input('Is it too high (H), too low (L), or correct (C): ').lower().strip()
+        attempts +=1
+        print('Tries: ',attempts)
+        
+    
