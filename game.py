57c1f876a0bd3476c82c52f197b87d62aedca722
Kay <126708934+Rakimzreal@users.noreply.github.com>
2025-07-06 23:48:50 +0200

    Put the tries and attempts after printing guess

--- game.py
+++ game.py
@@ -19,9 +19,9 @@ def computer_guess_game():
             return
         computer_guess = random.randint(low , high)
         print(f'Computer guessed {computer_guess}')
-        feedback = input('Is it too high (H), too low (L), or correct (C): ').lower().strip()
         attempts +=1
         print('Tries: ',attempts)
+        feedback = input('Is it too high (H), too low (L), or correct (C): ').lower().strip()
         
         if feedback == 'h':
             high = computer_guess - 1 
